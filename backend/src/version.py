5dd61a0ddfdc3c82f16d3d7983ad7b6c89251ab6
narqulie <Narqulie@gmail.com>
2025-04-18 08:39:02 +0300

    Version bumps whoopsie

--- backend/src/version.py
+++ backend/src/version.py
@@ -1,6 +1,6 @@
 """Version information for the application."""
 from loguru import logger
 
-__version__ = "1.5"
+__version__ = "1.5.1"
 
 logger.info(f"🚀 Starting application version {__version__}") 
